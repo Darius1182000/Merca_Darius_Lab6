70e5054227ae0652e323bd507dcfb289dbf39ce8
Darius1182000 <merca_darius@yahoo.ro>
2022-11-15 19:12:53 +0200

    Add project files.

---
+++ Models/ExpenseContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Merca_Darius_Lab6.Models
+{
+    public class ExpenseContext : DbContext
+    {
+        public ExpenseContext(DbContextOptions<ExpenseContext> options)
+ : base(options)
+        {
+        }
+        public DbSet<Expense> Expense { get; set; }
+
+    }
+}
